7d5c6e6da4ff3ce1cbd60587598e8c0562359c47
dnivedha <nivethadurairaju95@gmail.com>
2016-07-08 16:17:30 +0530

    finish

---
+++ README.c
@@ -0,0 +1,20 @@
+# my
+#include<stdio.h>
+#include<conio.h>
+main() 
+{
+
+int x,i=0;
+printf("Press 1 By For Loop:\nPress 2 By While Loop:\nPress 3 By Do while loop:\n");
+scanf("%d",&x);
+
+
+printf("\nPrinting of Hello World by using For Loop\n");
+
+for(i=0;i<5;i++)
+{
+printf("Hello World\n");
+}
+
+getch();
+}
